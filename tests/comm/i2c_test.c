6f47ab9b896394b0522792b484b7987773dbd1cd
Chris Choi[ <chutsu@gmail.com>
2016-10-14 02:48:37 +0000

    Add ability to read byte with i2c

--- tests/comm/i2c_test.c
+++ tests/comm/i2c_test.c
@@ -4,6 +4,7 @@
 
 /* TESTS */
 int test_i2c_setup(void);
+int test_i2c_read_byte(void);
 void test_suite(void);
 
 
@@ -16,9 +17,24 @@ int test_i2c_setup(void)
     return 0;
 }
 
+int test_i2c_read_byte(void)
+{
+    char data;
+    struct i2c conn;
+
+    i2c_setup(&conn);
+    i2c_set_slave(&conn, 0x68);
+    i2c_read_byte(&conn, 0x75, &data);
+   
+    printf("got: 0x%02X\n", (int) data);
+
+    return 0;
+}
+
 void test_suite()
 {
     mu_add_test(test_i2c_setup);
+    mu_add_test(test_i2c_read_byte);
 }
 
 mu_run_tests(test_suite)
